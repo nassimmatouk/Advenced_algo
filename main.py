adf940966a6e68ca40730c58e241a277f201fb1d
salimataana <ana.salimata.sanou@gmail.com>
2024-12-18 22:09:06 +0100

    Final fix

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ def main():
 
     """
     workbench = Worbench(algo_to_compare=[
-                                         Algorithm.BRUTE_FORCE,
+                                         #Algorithm.BRUTE_FORCE,
                                          Algorithm.MST_KRUSKAL,
-                                         Algorithm.BRANCH_BOUND,
+                                         #Algorithm.BRANCH_BOUND,
                                          Algorithm.MST_PRIM,
                                          Algorithm.NEAREST_NEIGHBOR,
                                          Algorithm.GREEDY,
                                          Algorithm.RANDOMIZED,
-                                         Algorithm.BRANCH_BOUND_CLASSIC
+                                         #Algorithm.BRANCH_BOUND_CLASSIC
 
                                                ],
-                                                interval_numbers_nodes=(3, 10),
+                                                interval_numbers_nodes=(3, 500),
                                                 symmetric=True,
                                                 #dataset=Dataset.RANDOM,
                                                 dataset=Dataset.A280,
